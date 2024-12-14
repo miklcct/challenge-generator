8700249a4b117519bffbd70167c337e435cbc892
Michael Tsang <miklcct@gmail.com>
2024-12-14 00:37:25 +0000

    update author

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@
 
   <footer>
     <p>
-      Copyright © 2024 Metroapps Limited. <a href="https://www.gnu.org/licenses/agpl-3.0-standalone.html">Licensed under GNU Affero General Public License, Version 3 or later.</a>
+      Copyright © 2024 Michael Tsang. <a href="https://www.gnu.org/licenses/agpl-3.0-standalone.html">Licensed under GNU Affero General Public License, Version 3 or later.</a>
       <br/>
-      Source code of this program can be viewed on <a href="https://github.com/metroapps/challenge-generator/">GitHub</a>.
+      Source code of this program can be viewed on <a href="https://github.com/miklcct/challenge-generator/">GitHub</a>.
     </p>
   </footer>
 </template>
